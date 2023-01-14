3b6dfe34bc3ec4671be35659b1c0fdb1dce780e8
ysf <yusuf.jkhan1@gmail.com>
2023-01-14 23:39:49 +0000

    Trained on A100

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 echo 'Begining Primary Installs'
-pip install -r requirements.txt
+pip install -qq git+https://github.com/ShivamShrirao/diffusers
+pip install -q -U --pre triton
+pip install -q accelerate==0.15.0 transformers==4.25.1 ftfy bitsandbytes==0.35.0 gradio natsort ninja
+pip install -v -U git+https://github.com/facebookresearch/xformers.git@47ab8b812fbc0518eb6b630440548c1ff7915500#egg=xformers
 
 echo 'Setup is expecting a .env file with a huggingface read token(hf_read_token)'
 echo 'Setting up huggingface token'
@@ -8,5 +11,7 @@ if [ -f .env ]
 then
     export $(cat .env | xargs)
     echo -n $hf_read_token > ~/.huggingface/token
+else
+    echo 'No .env file found'
 fi
 
